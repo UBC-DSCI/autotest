d508fdaa2e8733321f1388222ef4749d704ebd8b
Alireza Iranpour <alireza1iranpour@gmail.com>
2022-04-20 00:57:53 -0700

    Update instantiatetests.py
    added mechanism to load default tests if local tests.yaml file does not exist

--- preprocessors/instantiatetests.py
+++ preprocessors/instantiatetests.py
@@ -71,6 +71,11 @@ class InstantiateTests(Execute):
         help="The filename where automatic testing code is stored"
     ).tag(config=True)
 
+    default_autotest_filename = Unicode(
+        "default_tests.yml",
+        help="The filename where the default automatic testing code is stored"
+    ).tag(config=True)
+
     autotest_delimiter = Unicode(
         "AUTOTEST",
         help="The delimiter prior to snippets to be autotested"
@@ -234,10 +239,24 @@ class InstantiateTests(Execute):
             with open(os.path.join(resources['metadata']['path'], self.autotest_filename), 'r') as tests_file:
                 tests = yaml.safe_load(tests_file)
             self.log.debug(tests)
+
         except FileNotFoundError:
-            #if there is no tests file, just create a default empty tests dict
-            self.log.warning('No tests.yml file found. If AUTOTESTS appears in testing cells, an error will be thrown.')
-            tests = {}
+            # if there is no tests file, just load a default tests dict
+            self.log.warning('No tests.yml file found. Trying to load the default tests')
+            # tests = {}
+            try:
+                with open(os.path.join(self.default_autotest_filename), 'r') as tests_file:
+                    tests = yaml.safe_load(tests_file)
+            except FileNotFoundError:
+                # if there is no tests file, just create a default empty tests dict
+                self.log.warning(
+                    'No default_tests.yml file found. If AUTOTESTS appears in testing cells, an error will be thrown.')
+                tests = {}
+            except yaml.parser.ParserError as e:
+                self.log.error('tests.yml contains invalid YAML code.')
+                self.log.error(e.msg)
+                raise
+
         except yaml.parser.ParserError as e:
             self.log.error('tests.yml contains invalid YAML code.')
             self.log.error(e.msg)
