2b4d2a6097b2a1ce3768ceb10456c6b8844a364b
Phil Austin <you@example.comw>
2022-05-20 14:53:59 -0700

    docker init

---
+++ docker_files/nbgrader_image/autotest_files/install_autotest.sh
@@ -0,0 +1,16 @@
+if ! type nbgrader
+then
+    echo "nbgrader-0.6.2 or higher was not installed"
+fi
+
+nbgrader_path=$(pip show nbgrader | grep "^Location:*" | cut -c11-)
+echo $nbgrader_path
+
+cp converters/generate_assignment.py $nbgrader_path/nbgrader/converters
+cp preprocessors/instantiatetests.py $nbgrader_path/nbgrader/preprocessors
+cp preprocessors/limittestcellheights.py $nbgrader_path/nbgrader/preprocessors
+cp preprocessors/__init__.py $nbgrader_path/nbgrader/preprocessors
+cp preprocessors/execute.py $nbgrader_path/nbgrader/preprocessors
+
+cp -r nbextensions/limit_cell_height $nbgrader_path/nbgrader/nbextensions
+cp __init__.py $nbgrader_path/nbgrader
---
+++ docker_files/nbgrader_image/jupyter_notebook_config.py
@@ -0,0 +1,10 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+c = get_config()  # noqa
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.password  = u''
+
+
