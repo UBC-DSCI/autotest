7023cd917b0f8a9fa51ade980c5181efd1a10f4d
Trevor Campbell <trevor.d.campbell@gmail.com>
2022-05-23 15:56:06 -0700

    moved docker to demo folder; cleanup to remove as much non-autotest-related code as possible

--- docker_files/bringdown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -v
-docker-compose down
-docker stop $(docker ps -aq)
-docker rm $(docker ps -a -q)
-docker volume rm $(docker volume ls -q)
-docker network prune -f
-#docker rmi $(docker images -q)
